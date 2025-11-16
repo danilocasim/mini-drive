de4f7187314c7f0189a294f18ae7cad3af570d6a
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-16 21:35:01 +0800

    feat: add user queries to get email, user by id, and create user

---
+++ prisma/queries/UserQueries.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("../../generated/prisma");
+const prisma = new PrismaClient();
+
+class UserQueries {
+  async getEmail(email) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    return user;
+  }
+
+  async getUser(id) {
+    const user = prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  }
+
+  async addUser({ fullname, email, password }) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        fullname: fullname,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
+
+module.exports = new UserQueries();
