1665c210f04e20063f9fe484ff91adbca04dc60e
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-16 21:33:56 +0800

    feat: add passport configuration for local strategy

---
+++ configs/passport.js
@@ -0,0 +1,39 @@
+const db = require("../prisma/queries/UserQueries");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcryptjs");
+
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const user = await db.getEmail(email);
+      console.log("user");
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect Email" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUser(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
