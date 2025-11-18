f5148c93e4fa0e358688cde17acd7164ea261cf4
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-18 11:27:44 +0800

    fix: LocalStrategy default field was username so configuring the username to email fix the bug

--- configs/passport.js
+++ configs/passport.js
@@ -4,25 +4,27 @@ const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
 passport.use(
-  new LocalStrategy(async (email, password, done) => {
-    try {
-      const user = await db.getEmail(email);
-      console.log(user);
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await db.getEmail(email);
 
-      if (!user) {
-        return done(null, false, { message: "Incorrect Email" });
+        if (!user) {
+          return done(null, false, { message: "Incorrect Email" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
-
-      const match = await bcrypt.compare(password, user.password);
-
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
     }
-  })
+  )
 );
 
 passport.serializeUser((user, done) => {
