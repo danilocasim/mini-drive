996b72dd9c4a45ee2621b0823b64c8c491c4e338
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-19 14:06:50 +0800

    fix: remove controller functionality from router to follow MVC approach

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,18 +1,6 @@
 const { Router } = require("express");
 const { addUser, logout } = require("../controller/userController");
 const passport = require("passport");
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
 const userRouter = Router();
 
 userRouter.post("/signup", addUser);
@@ -26,11 +14,5 @@ userRouter.post(
 );
 
 userRouter.get("/logout", logout);
-
-userRouter.post("/profile", upload.single("avatar"), (req, res, next) => {
-  console.log(req.file);
-
-  res.redirect("/");
-});
 
 module.exports = userRouter;
