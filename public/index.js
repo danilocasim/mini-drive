2b7bdb056da08fcefa9a3c4956fa453d22eb4c34
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-12-04 15:15:49 +0800

    fix: typo for showModal

--- public/index.js
+++ public/index.js
@@ -2,5 +2,5 @@ const addDriveDialog = document.getElementById("addDriveDialog");
 const addDriveBtn = document.getElementById("addDriveBtn");
 
 addDriveBtn.addEventListener("click", () => {
-  addDriveDialog.showModel();
+  addDriveDialog.showModal();
 });
